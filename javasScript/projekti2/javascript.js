bcef52eb1c4e63b8b874379de014167969c7c662
TiinaKop <tiinaKop>
2021-11-26 15:32:07 +0200

    laskin

--- javasScript/projekti2/javascript.js
+++ javasScript/projekti2/javascript.js
@@ -1,6 +1,12 @@
+
+
 document.getElementById("btn").addEventListener("click", myFunction);
 
 function myFunction() {
+
+
+
+
   let annaNumero = document.getElementById("annaNumero").value;
   let random = [Math.floor(Math.random()* 10)]
   if (annaNumero == random) {
@@ -12,4 +18,21 @@ function myFunction() {
   else if (annaNumero < random) {
       document.getElementById("demo").innerHTML = "Arvauksesi on liian pieni";
   }
+
+  var yhteens = 1;
+  var pisteet = 0;
+
+  var k1 = document.getElementById("annaNumero")
+
+  var vastaukset = ["random"];
+
+  for(i = 1; i <= yhteensa; i++)
+  {
+    if(eval("annaNumero"+i) == vastaukset[i-1])
+  }
+    pisteet++;
+  }
+
+var tulokset = document.getElementById("tulokset");
+tulokset.innerHTML = "<h3>Sait " + pisteet + "pistettä</h3>"
 }
