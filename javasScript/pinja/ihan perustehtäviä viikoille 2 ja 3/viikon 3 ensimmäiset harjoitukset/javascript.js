dc74457458f1d4536e42f7585d1e4e9e807c69d8
TiinaKop <tiinaKop>
2021-12-15 13:48:59 +0200

    tehtäviä

--- javasScript/pinja/ihan perustehtäviä viikoille 2 ja 3/viikon 3 ensimmäiset harjoitukset/javascript.js
+++ javasScript/pinja/ihan perustehtäviä viikoille 2 ja 3/viikon 3 ensimmäiset harjoitukset/javascript.js
@@ -1,7 +1,32 @@
-function tehtava1() {
-  var x = document.getElementById('annaLuku').value;
-
-  for (var i = 0; i <= x ; i++) {
-    document.getElementById('tulostaLuvut').innerHTML = i;
+function tehtava() {
+  var x = parseInt(document.getElementById('annaLuku').value);
+  var jono = "<p>";
+  for(var i = 2; i <= x; i = i+2) {
+    jono+=i;
+    jono+=" ";
   }
+    jono+="</p>";
+    document.getElementById('tulostaLuvut').innerHTML = jono;
+  }
+
+function luvut() {
+  var y = parseInt(document.getElementById('luku').value);
+  var uusi = "<p>";
+  for (var i = 1; i <= y; i= i+2) {
+    uusi+=i;
+    uusi+=" ";
+  }
+  uusi+="</p>";
+  document.getElementById('luvut').innerHTML = uusi;
+}
+
+function salasana() {
+  var sana = document.getElementById('annaSana').value;
+  var uusit = "<p>";
+  for (var j = 0; j < sana.length; j++) {
+    uusit+=sana[j];
+    uusit+="Ö";
+  }
+  uusit+="</p>";
+  document.getElementById('tulostaSana').innerHTML = uusit;
 }
