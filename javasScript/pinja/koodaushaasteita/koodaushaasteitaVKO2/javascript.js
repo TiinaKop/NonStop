1b34061faac0334b038b179b016f1452ad40ffeb
TiinaKop <tiinaKop>
2022-01-12 14:22:30 +0200

    harjoituksia

--- javasScript/pinja/koodaushaasteita/koodaushaasteitaVKO2/javascript.js
+++ javasScript/pinja/koodaushaasteita/koodaushaasteitaVKO2/javascript.js
@@ -1,5 +1,56 @@
 function taulukko() {
-  taulu = [8, 4, 9, 6, 2];
-  document.getElementById('taulukko').innerHTML =
-  '<p>Pienin luku: ' + taulu[0] + ' ja suurin luku: '+ taulu[taulu.length-1];
+document.getElementById('pieni').innerHTML = "Pienin numero on " + Math.min(8, 4, 9, 6, 2);
+document.getElementById('suuri').innerHTML = "Suurin numero on " + Math.max(8, 4, 9, 6, 2);
+}
+
+function Paritonko() {
+  var n = document.getElementById('numero').value;
+  if (n%2 == 0)
+    document.getElementById('vastaus').innerHTML = "Numero on parillinen";
+  else {
+    document.getElementById('vastaus').innerHTML = "Numero on pariton";
+  }
+}
+
+function kuukausi() {
+var text;
+var day = document.getElementById('kuukausi').value;
+switch (day) {
+  case "1":
+    text = "Tammikuu";
+    break;
+  case "2":
+    text = "Helmikuu";
+    break;
+  case "3":
+    text = "Maaliskuu";
+    break;
+  case "4":
+    text = "Huhtikuu";
+    break;
+  case "5":
+    text = "Toukokuu";
+    break;
+  case "6":
+    text = "Kesäkuu";
+    break;
+  case "7":
+    text = "Heinäkuu";
+    break;
+  case "8":
+    text = "Elokuu";
+    break;
+  case "9":
+    text = "Syyskuu";
+    break;
+  case "10":
+    text = "Lokakuu";
+    break;
+  case "11":
+    text = "Marraskuu";
+    break;
+  case "12":
+    text = "Joulukuu";
+}
+document.getElementById('TulostaKuukausi').innerHTML = text;
 }
