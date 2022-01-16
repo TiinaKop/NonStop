c6e0086325504beebb5d5692dffa34240b858b3b
TiinaKop <tiinaKop>
2022-01-16 11:23:35 +0200

    ..

--- javasScript/kokeiluja/alert/kokeilu/kokeiluUusi/kokeilu.js
+++ javasScript/kokeiluja/alert/kokeilu/kokeiluUusi/kokeilu.js
@@ -38,32 +38,96 @@ function kolmeSanaaUusi() {
   document.getElementById('TulostaSanatUusi').innerHTML = sana.repeat(3);
 }
 
-function numerotJarjestykseen(() {
-  var yksi = document.getElementById('yksi').value;
-  var kaksi = document.getElementById('kaksi').value;
-  var kolme = document.getElementById('kolme').value;
+function numerotJarjestykseen() {
+  var yksi = parseInt(document.getElementById('yksi').value);
+  var kaksi = parseInt(document.getElementById('kaksi').value);
+  var kolme = parseInt(document.getElementById('kolme').value);
+
   if (yksi < kaksi && yksi < kolme) {
     if (kaksi < kolme) {
-      document.getElementById('jarjestaNumerot').innerHTML = yksi + kaksi + kolme;
+      document.getElementById('jarjestaNumerot').innerHTML = yksi + " " + kaksi + " " + kolme;
     }
     else {
-      document.getElementById('jarjestaNumerot').innerHTML = yksi + kolme + kaksi;
+      document.getElementById('jarjestaNumerot').innerHTML = yksi + " " + kolme + " " + kaksi;
     }
   }
   else if (kaksi < kolme && kaksi < yksi) {
     if (kolme < yksi) {
-      document.getElementById('jarjestaNumerot').innerHTML = kaksi + kolme + yksi;
+      document.getElementById('jarjestaNumerot').innerHTML = kaksi + " " + kolme + " " + yksi;
     }
     else {
-      document.getElementById('jarjestaNumerot').innerHTML = kaksi + yksi + kolme;
+      document.getElementById('jarjestaNumerot').innerHTML = kaksi + " " + yksi + " " + kolme;
     }
   }
   else if (kolme < kaksi && kolme < yksi) {
     if (kaksi < yksi) {
-      document.getElementById('jarjestaNumerot').innerHTML = kolme + kaksi + yksi;
+      document.getElementById('jarjestaNumerot').innerHTML = kolme + " " + kaksi + " " + yksi;
     }
     else {
-      document.getElementById('jarjestaNumerot').innerHTML = kolme + yksi + kaksi;
+      document.getElementById('jarjestaNumerot').innerHTML = kolme + " " + yksi + " " + kaksi;
     }
 }
+}
+
+/*function pieninSuurin() {
+  var eka, toka, kolmas;
+  eka = parseInt(document.getElementById('eka').value);
+  toka = parseInt(document.getElementById('toka').value);
+  kolmas = parseInt(document.getElementById('kolmas').value);
+  document.getElementById('tulostaP').innerHTML = "Pienin numero on " + Math.min(eka, toka, kolmas);
+  document.getElementById('tulostaS').innerHTML = "Suurin numero on " + Math.max(eka, toka, kolmas);
+}*/
+
+function pieninSuurin() {
+  var eka, toka, kolmas;
+  eka = parseInt(document.getElementById('eka').value);
+  toka = parseInt(document.getElementById('toka').value);
+  kolmas = parseInt(document.getElementById('kolmas').value);
+if (eka < toka && kolmas < toka) {
+  if (eka < kolmas) {
+    document.getElementById('tulostaS').innerHTML = "Suurin numero on " + toka + "<br> Pienin numero on " + eka;
+  }
+  else {
+      document.getElementById('tulostaS').innerHTML = "Suurin numero on " + toka + "<br> Pienin numero on " + kolmas;
+  }
+}
+else if (toka < kolmas && eka < kolmas) {
+  if (toka < eka) {
+    document.getElementById('tulostaS').innerHTML = "Suurin numero on " + kolmas + "<br> Pienin numero on " + toka;
+  }
+  else {
+      document.getElementById('tulostaS').innerHTML = "Suurin numero on " + kolmas + "<br> Pienin numero on " + eka;
+  }
+}
+else if (kolmas < eka && toka < eka) {
+  if (kolmas < toka) {
+    document.getElementById('tulostaS').innerHTML = "Suurin numero on " + eka + "<br> Pienin numero on " + kolmas;
+    }
+  else {
+    document.getElementById('tulostaS').innerHTML = "Suurin numero on " + eka + "<br> Pienin numero on " + toka;
+    }
+  }
+}
+
+function PariPariton() {
+  var numero = parseInt(document.getElementById('Numero').value);
+  if (numero%2 == 0) {
+    document.getElementById('tulostaPari').innerHTML = "Numero on parillinen";
+  }
+  else {
+    document.getElementById('tulostaPari').innerHTML = "Numero on pariton";
+  }
+}
+
+function millaAjaa() {
+  var ika = parseInt(document.getElementById('ika').value);
+  if (ika >= 18) {
+    document.getElementById('tulostaMenopeli').innerHTML = "Voit ajaa autolla";
+  }
+  else if (ika >= 16) {
+    document.getElementById('tulostaMenopeli').innerHTML = "Voit ajaa mopolla";
+  }
+  else {
+    document.getElementById('tulostaMenopeli').innerHTML = "Voit ajaa polkupyörällä";
+  }
 }
