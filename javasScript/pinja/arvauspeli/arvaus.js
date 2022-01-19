5e065048c38c04c1676b937bceab7a1f55dd4e39
TiinaKop <tiinaKop>
2022-01-19 14:35:54 +0200

    arvauspeliä

--- javasScript/pinja/arvauspeli/arvaus.js
+++ javasScript/pinja/arvauspeli/arvaus.js
@@ -1,13 +1,37 @@
-function arvauspeli() {
+function numero() {
+  return Math.floor(Math.random()* 10 + 1);
+}
+
+var numero = numero();
 var arvaus = document.getElementById('arvaus').value;
-var x = Math.floor(Math.random()*10 +1);
-if (arvaus == x) {
-  document.getElementById('vastaus').innerHTML = "oikein";
+var pisteet = 0;
+var kerrat = 1;
+
+function arvauspeli() {
+  document.getElementById('arvauskerta').innerHTML = kerrat;
+  document.getElementById('pisteet').innerHTML = pisteet;
+
+if (kerrat <= 3) {
 }
-else if (arvaus < x) {
+if (arvaus == numero) {
+  document.getElementById('vastaus').innerHTML = "oikein. aloitetaan uusi peli";
+  kerrat = 1;
+  pisteet++;
+  numero = numero();
+}
+else if (arvaus >= 3) {
+  document.getElementById('alusta').innerHTML = "Arvasit 3 kertaa";
+  kerrat = 1;
+  numero = numero();
+}
+else if (arvaus < numero) {
   document.getElementById('vastaus').innerHTML = "liian pieni";
+  kerrat++;
 }
-else if (arvaus > x) {
+else if (arvaus > numero) {
   document.getElementById('vastaus').innerHTML = "liian suuri";
+  kerrat++;
 }
+document.getElementById('arvauskerta').innerHTML = kerrat;
+document.getElementById('pisteet').innerHTML = pisteet;
 }
