e21719ca2bf7e41822aa83dc995b7fbeb825cf42
TiinaKop <tiinaKop>
2022-02-02 14:36:56 +0200

    ..

--- javasScript/pinja/koodaushaasteita/koodaushaasteitaVKO2/javascript.js
+++ javasScript/pinja/koodaushaasteita/koodaushaasteitaVKO2/javascript.js
@@ -56,94 +56,40 @@ document.getElementById('TulostaKuukausi').innerHTML = text;
 }
 
 
-function tieto(etunimi, sukunimi, puh, osoite) {
-  this.etunimi = etunimi,
-  this.sukunimi = sukunimi,
-  this.puh = puh,
-  this.osoite = osoite
+
+
+
+
+function henkilo(etunimi, sukunimi, puh, osoite) { // tehtävä 4
+  this.etunimi = etunimi;
+  this.sukunimi = sukunimi;
+  this.puh = puh;
+  this.osoite = osoite;
+  this.tiedot = tulosta;
 }
-var Marika = new tieto("Marika", "Koponen", "0407432100", "Katu 1");
-console.log(Marika.puh);
+
+function tulosta() {
+  var etunimi = this.etunimi;
+  var sukunimi = this.sukunimi;
+  var puh = this.puh;
+  var osoite = this.osoite;
+
+  document.write("hei" + etunimi); // ei toimi
+}
+
+var Marika = new henkilo("Marika", "Koponen", 0407432100, "Katu 1");
+var Kaija = new henkilo("Kaija", "Hoonen", 040744444, "Katu 5");
+var Milla = new henkilo("Milla", "hasunen", 0409999999, "Katu 10");
 
 
-function Sana(sana) {
-  var y = Array.from(sana);
-  y.sort();
-  var text = y.join('');
+
+
+
+
+
+function sana(teksti) {
+  var taulukkoteksti = Array.from(teksti);
+  taulukkoteksti.sort();
+  var text = taulukkoteksti.join('');
   console.log(text);
 }
-
-function arvosana() {
-  var taulu = [], temp;
-  for (var i = 0; i < 5; i++) {
-    temp = document.getElementById('A['+i+']').value;
-    taulu.push(temp);
-  }
-  var nimi = [], temp;
-  for (var j = 0; j < 5; j++) {
-    temp = document.getElementById('B['+j+']').value;
-    nimi.push(temp);
-  }
-  if (taulu < 50) {
-    console.log(nimi + "hylätty");
-  }
-  else if (taulu < 60) {
-    console.log(nimi + "T1");
-  }
-  else if (taulu < 70) {
-    console.log(nimi + "T2");
-  }
-  else if (taulu < 80) {
-    console.log(nimi + "H3");
-  }
-  else if (taulu < 90) {
-    console.log(nimi + "H4");
-  }
-  else if (taulu < 100) {
-    console.log(nimi + "H5");
-  }
-  document.getElementById('arvosana').innerHTML = taulu + nimi;
-}
-
-function Grade(){
-  var sub1 = parseInt(document.getElementById("eng").value);
-  var sub2 = parseInt(document.getElementById("mat").value);
-  var sub3 = parseInt(document.getElementById("phy").value);
-  var sub4 = parseInt(document.getElementById("chm").value);
-  var sub5 = parseInt(document.getElementById("cmp").value);
-
-   if(sub1>100 || sub2>100 || sub3>100 || sub4>100 || sub5>100 )
-  {
-    alert("Please Enter Marks in range of 100");
-  }else {
-     var total= sub1 + sub2 + sub3 + sub4 + sub5;
-     var avg=total/5;
-
-     if(avg>=80 && avg<=100)
-     {
-       document.getElementById("grade").innerHTML="You Got A+ Grade";
-     }
-     else if(avg>=75 && avg<=80)
-     {
-       document.getElementById("grade").innerHTML="You Got A+ Grade";
-     }
-     else if(avg>=70 && avg<=75)
-     {
-       document.getElementById("grade").innerHTML="You Got A Grade";
-     }
-     else if(avg>=65 && avg<=70)
-     {
-       document.getElementById("grade").innerHTML="You Got B Grade";
-     }
-     else if(avg>=50 && avg<=60)
-     {
-       document.getElementById("grade").innerHTML="You Got C Grade";
-     }
-     else if(avg>=40 && avg<=50)
-     {
-       document.getElementById("grade").innerHTML="You Got C Grade";
-     }
-
-     else {
-       document.getElementById("grade").innerHTML="You Got F Grade";
-     }
