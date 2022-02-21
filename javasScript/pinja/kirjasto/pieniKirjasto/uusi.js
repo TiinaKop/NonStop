f35e8640e73324cc4602c41385001dee4e9d3b0d
TiinaKop <tiinaKop>
2022-02-21 10:25:34 +0200

    .

---
+++ javasScript/pinja/kirjasto/pieniKirjasto/uusi.js
@@ -0,0 +1,7 @@
+function m() {
+var x = parseInt(document.getElementById('y').value);
+if (x.length !=2) {
+  alert("Syötä postinumero 5 numeroa");
+  return false;
+}
+}
--- javasScript/pinja/kirjasto/projekti6/kirjasto.js
+++ javasScript/pinja/kirjasto/projekti6/kirjasto.js
@@ -3,46 +3,81 @@ function tallenna() {
   var etunimi = document.getElementById('etunimi').value;
   var sukunimi = document.getElementById('sukunimi').value;
   var osoite = document.getElementById('lahiosoite').value;
-  var postinumero = parseInt(document.getElementById('postinumero').value);
+  var postinumero = document.getElementById('postinumero').value;
   var postitoimipaikka = document.getElementById('postitoimipaikka').value;
-  var puhelin = parseInt(document.getElementById('puhelin').value);
+  var puhelin = document.getElementById('puhelin').value;
   var sahkoposti = document.getElementById('sposti').value;
   var key = document.getElementById('key').value;
 
-  if (etunimi == "" || sukunimi == "" || osoite == "" || postinumero == "" || postitoimipaikka == "" || puhelin == "" || sahkoposti == "" || key == "") {
-    alert("Täytä tyhjät kentät")
+  if (key == "") {
+    alert("Syötä avain");
     return false;
   }
 
-  if (etunimi.length<2 || sukunimi.length<2 || postitoimipaikka.length<2) {
-    alert("Tarkista nimet ja postitoimipaikka");
+  if (etunimi == "" || etunimi.length<2) {
+    alert("Syötä etunimi");
     return false;
   }
 
-  if (/\d/.test(etunimi) || /\d/.test(sukunimi)) { // tarkistaa sisältääkö numeroita
-    alert("Syötä nimiin vain tekstiä");
+  if (/\d/.test(etunimi)) { // tarkistaa sisältääkö numeroita
+    alert("Syötä etunimeen vain tekstiä");
+    return false;
+  }
+
+  if (sukunimi == "" || sukunimi.length<2) {
+    alert("Syötä sukunimi");
+    return false;
+  }
+
+  if (/\d/.test(sukunimi)) { // tarkistaa sisältääkö numeroita
+    alert("Syötä sukunimeen vain tekstiä");
+    return false;
+  }
+
+  if (osoite == "" || osoite.length<2) {
+    alert("Syötä osoite");
+    return false;
+  }
+
+  if (postinumero == "" || postinumero.length !=5) {
+    alert("Syötä postinumero (5 numeroa)");
+    return false;
+  }
+
+  if (/[a-zA-Z]/.test(postinumero)) { // tarkistaa sisältääkö tekstiä
+    alert("Syötä postinumeroon vain numeroita");
+    return false;
+  }
+
+  if (postitoimipaikka == "" || postitoimipaikka.length<2) {
+    alert("Syötä postitoimipaikka");
+    return false;
+  }
+
+  if (puhelin == "" || puhelin.length !=10) {
+    alert("Syötä puhelinnumero muodossa 0401234567");
+    return false;
+  }
+
+  if (/[a-zA-Z]/.test(puhelin)) { // tarkistaa sisältääkö tekstiä
+    alert("Syötä puhelinnumeroon vain numeroita");
+    return false;
+  }
+
+  if (sahkoposti == "") {
+    alert("Syötä sähköpostiosoite");
     return false;
   }
 
   if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(sahkoposti)) { //tarkistaa onko sähköpostiosoite muodossa a@b.cde ja onko siinä välilyöntejä
-    alert("annoit sähköpostiosoitteesi oikein")
+    alert("annoit sähköpostiosoitteesi oikein");
   }
   else {
-    alert("Anna oikea sähköpostiosoitteesi");
+    alert("Anna sähköpostiosoite muodossa marika.koponen@gmail.com");
     return false;
   }
 
 
-
-  if (/[a-zA-Z]/.test(puhelin)) { // tarkistaa sisältääkö tekstiä
-    alert("Syötä puhelimeen vain numeroita");
-    return false;
-  }
-
-  if (/[a-zA-Z]/.test(postinumero)) {
-    alert("Syötä postinumeroon vain numeroita");
-    return false;
-  }
 
 
   const tiedot = { // objekti
@@ -90,8 +125,7 @@ function poistaKaikki() {
 }
 
 
-window.onload =function(){ //varmistaa, että sivu ladataan ennen toimintojen suorittamista.
-    //document.getElementById("pehmot").onsubmit = tallenna
+window.onload = function(){ //varmistaa, että sivu ladataan ennen toimintojen suorittamista.
     document.getElementById("etsi").onclick = find
     document.getElementById("poista").onclick = poista
     document.getElementById("poistaKaikki").onclick = poistaKaikki
