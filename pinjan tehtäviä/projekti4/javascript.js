69e7cc9eab14393e1eda1b53cd27e6c86d4f4b6a
TiinaKop <tiinaKop>
2022-02-26 11:29:46 +0200

    ..

--- pinjan tehtäviä/projekti4/javascript.js
+++ pinjan tehtäviä/projekti4/javascript.js
@@ -1,53 +1,53 @@
-var myNodelist = document.getElementsByTagName("LI"); //
-for (var i = 0; i < myNodelist.length; i++) { // alkaen nollasta, päättyy ,myNodelistoom
-  var span = document.createElement("SPAN"); // luodaan span elementti
-  var txt = document.createTextNode("\u00D7"); // luodaan teksti
-  span.className = "close"; // luodaan span:lle className
-  span.appendChild(txt); // liitetään teksti spaniin
-  myNodelist[i].appendChild(span);
-}
 
 
-// Piitottaa tehtävän, kun painaa X painiketta
-var close = document.getElementsByClassName("close"); // haetaan yllä luotu class close
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() { tehdään functio
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// tehtävä tulee tehdyksi, kun painaa tehtävää
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) { // lisää tapahtuman hiiren klikkaus, toimii functiolla ev
-  if (ev.target.tagName === 'LI') { // Jos on valittu li
-    ev.target.classList.toggle('checked'); // saa valittua pois ja päälle (checked määritellään css:ssä, mikä muuttuu kun painaa)
-  }
-}, false);
-
-// lisää uuden tehtävän, kun painaa add
-function newElement() {
-  var li = document.createElement("li"); //Luodaan li elementti
-  var inputValue = document.getElementById("myInput").value; // haetaan käyttäjän kirjoittama teksti
-  var t = document.createTextNode(inputValue); // luodaan tekstiä
-  li.appendChild(t); // liitetään teksti li elementtiin
-  if (inputValue === '') {  //
-    alert("You must write something!");
+// lisää uuden tehtävän, kun painaa lisää
+function uusiTehtava() {
+  var lista = document.createElement("li"); //Luodaan li elementti
+  var kayttajanTehtava = document.getElementById("kayttajanTehtava").value; // haetaan käyttäjän kirjoittama teksti
+  var teksti = document.createTextNode(kayttajanTehtava); // luodaan teksti
+  lista.appendChild(teksti); // liitetään teksti li elementtiin
+  if (kayttajanTehtava === '') {  // käyttäjä painaa liitä, muttei ole tekstiä
+    alert("Kirjoita tehtävä");
   } else {
-    document.getElementById("myUL").appendChild(li); // liitetään teksti myUL elementtiin
+    document.getElementById("tehtavat").appendChild(lista); // liitetään teksti tehtavat elementtiin
   }
-  document.getElementById("myInput").value = ""; // tyhjennetään teksti syöttölaatikosta
+  document.getElementById("kayttajanTehtava").value = ""; // tyhjennetään teksti syöttölaatikosta
 
+  // Luodaan X jokaiseen tehtävään
   var span = document.createElement("SPAN"); // ludaan span elementti
-  var txt = document.createTextNode("\u00D7"); // ludaan tekstiä (X)
+  var ruksi = document.createTextNode("\u00D7"); // ludaan teksti (X)
   span.className = "close"; // luodaan className span:lle
-  span.appendChild(txt); // liitetään teksti span:n
-  li.appendChild(span); // liitetään span li elementtiin
+  span.appendChild(ruksi); // liitetään teksti span:n
+  lista.appendChild(span); // liitetään span li elementtiin
 
+  // Luodaan toiminto, joka poistaa tehtävän painamalla X
+  var close = document.getElementsByClassName("close"); // haetaan className close
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() { // tehdään functio
-      var div = this.parentElement;
-      div.style.display = "none"; // poistaa (X) taustavärin
+    close[i].onclick = function() { // luodaan functio, joka toimii napauttamalla
+      var div = this.parentNode;
+      div.style.display = "none";
     }
   }
 }
+
+// Luodaan X painike jokaiseen tehtävään
+/*var myNodelist = document.getElementsByTagName("LI"); //
+for (var i = 0; i < myNodelist.length; i++) { // alkaen nollasta, päättyy myNodelistiin
+  var span = document.createElement("SPAN"); // luodaan span elementti
+  var ruksi = document.createTextNode("\u00D7"); // luodaan teksti (x)
+  span.className = "close"; // luodaan span:lle className
+  span.appendChild(ruksi); // liitetään teksti spaniin
+  myNodelist[i].appendChild(span);
+}*/
+
+
+// Poistaa tehtävän, kun painaa X painiketta
+
+
+// Napauttamalla tehtävää se tulee tehdyksi
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) { // lisää tapahtuman hiiren klikkaus, toimii functiolla ev
+  if (ev.target.tagName === 'LI') { // Jos elementti on li
+    ev.target.classList.toggle('checked'); // saa valittua pois ja päälle (checked määritellään css:ssä, mikä muuttuu)
+  }
+}, false);
